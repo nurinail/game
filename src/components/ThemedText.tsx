1859e159c5d8071265622891dd6a8f82460eaafc
nail.nuri <nail.nuri@iktex.az>
2026-04-05 14:21:41 +0400

    feat/project updated

--- src/components/ThemedText.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Text, TextProps } from 'react-native';
-
-const ThemedText: React.FC<TextProps> = (props) => {
-  return <Text {...props} />;
-};
-
-export default ThemedText;
--- src/components/ThemedView.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { View, ViewProps } from 'react-native';
-
-const ThemedView: React.FC<ViewProps> = (props) => {
-  return <View {...props} />;
-};
-
-export default ThemedView;
